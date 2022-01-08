4774a402d7ea10eff18639a64ecb5448682568da
ndmt1at21 <trminh36@gmail.com>
2022-01-08 08:13:14 +0700

    fix warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AlertNotification from './components/AlertNotification';
 import { AlertProvider } from './context/AlertContext';
-import AdminLayout from './layouts/AdminLayout';
-import AddAdminPage from './pages/AddAdminPage';
-import ClassManagePage from './pages/ClassManagePage';
-import LoginPage from './pages/LoginPage';
-import UserManagePage from './pages/UserManagePage';
-import ViewAdminPage from './pages/ViewAdminsPage';
 import AppRoutes from './routes';
 
 function App() {
