6d17da97745613fd7ae5000e62ff6e7d4a69fa7a
ndmt1at21 <trminh36@gmail.com>
2022-01-18 22:09:51 +0700

    fix width height 404page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -3,7 +3,7 @@ import svg404 from '../../assets/404.svg';
 import Button from '../../components/Button';
 
 const NotFoundPage = () => (
-	<div className="flex flex-col items-center justify-center w-screen h-screen">
+	<div className="flex flex-col items-center justify-center w-full h-full">
 		<div aria-label="image404" className="mt-10 md:w-96 w-72">
 			<img src={svg404} alt="404" className="w-full h-full" />
 		</div>
